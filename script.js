62037fbd7afc911a7145bff32d1aff2f652a7fa0
Nicolás Vera Zúñiga <nicovera@quetru.cl>
2024-01-03 13:48:15 -0300

    Add slideshow functionality and update styles

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentYear = new Date().getFullYear();
     document.getElementById('current-year').textContent = currentYear;
 });
+
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+}
+
+let slideIndex = 0;
+showSlides(); // Start the slideshow
+
+function showSlides() {
+    let slides = document.getElementsByClassName("carousel-slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} // Loop back to the first slide
+    slides[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 6000); // Change slide every 3 seconds
+}
