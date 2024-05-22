ec1efef51b3ffdf61429da2420f615d526d00a99
Nicolás Vera Zúñiga <nicovera@quetru.cl>
2024-05-22 15:45:02 -0400

    carousel

--- script.js
+++ script.js
@@ -4,23 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentYear = new Date().getFullYear();
     document.getElementById('current-year').textContent = currentYear;
 
-    let currentSlide = 0;
-    const slides = document.querySelectorAll('.carousel-slide');
-    const totalSlides = slides.length;
+    function initializeCarousel(carouselClass) {
+        let currentSlide = 0;
+        const slides = document.querySelectorAll(`.${carouselClass}`);
+        const totalSlides = slides.length;
 
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            slide.style.opacity = i === index ? '1' : '0';
-        });
+        function showSlide(index) {
+            slides.forEach((slide, i) => {
+                slide.style.opacity = i === index ? '1' : '0';
+            });
+        }
+
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % totalSlides;
+            showSlide(currentSlide);
+        }
+
+        showSlide(currentSlide); // Show the first slide initially
+        setInterval(nextSlide, 3000); // Change slide every 3 seconds
     }
 
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % totalSlides;
-        showSlide(currentSlide);
-    }
-
-    setInterval(nextSlide, 3000); // Change slide every 3 seconds
-    showSlide(currentSlide); // Show the first slide initially
+    // Initialize each carousel
+    initializeCarousel('carousel-slide-mahindra');
+    initializeCarousel('carousel-slide-peugeot');
+    initializeCarousel('carousel-slide-grua');
 });
 
 window.onbeforeunload = () => {
@@ -29,16 +36,8 @@ window.onbeforeunload = () => {
     }
 }
 
-// let slideIndex = 0;
-// showSlides(); // Start the slideshow
-
-// function showSlides() {
-//     let slides = document.getElementsByClassName("carousel-slide");
-//     for (let i = 0; i < slides.length; i++) {
-//         slides[i].classList.remove("active");
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {slideIndex = 1} // Loop back to the first slide
-//     slides[slideIndex - 1].classList.add("active");
-//     setTimeout(showSlides, 6000); // Change slide every 3 seconds
-// }
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+}
